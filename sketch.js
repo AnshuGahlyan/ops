4a4d5872e7b89fdf9b3025080182b73aeef4f664
AnshuGahlyan <71934013+AnshuGahlyan@users.noreply.github.com>
2021-07-28 15:25:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+
+
+
+function preload(){
+
+ 
+
+}
+function setup() {
+  createCanvas(1340,640);
+  
+ 
+
+}
+
+
+function draw() {
+
+  window.open("https://anshugahlyan.github.io/ops/");
+  background(0);
+  drawSprites();
+
+   }
+
+     
+
+  
